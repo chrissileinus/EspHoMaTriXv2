d543eb4a8371b8c94fa6ce594f41bf55b0c87887
Christian Backus <chrissileinus@googlemail.com>
2024-12-18 13:15:53 +0100

    dfhdf

--- components/ehmtxv2/EHMTX.cpp
+++ components/ehmtxv2/EHMTX.cpp
@@ -330,9 +330,7 @@ namespace esphome
     register_service(&EHMTX::force_screen, "force_screen", {"icon_name", "mode"});
 
     register_service(&EHMTX::full_screen, "full_screen", {"icon_name", "lifetime", "screen_time"});
-    register_service(&EHMTX::icon_screen, "icon_screen", {"icon_name", "text", "lifetime", "screen_time"});
-    // register_service(&EHMTX::icon_screen, "icon_screen", {"icon_name", "text", "lifetime", "screen_time", "default_font"});
-    // register_service(&EHMTX::icon_screen, "icon_screen", {"icon_name", "text", "lifetime", "screen_time", "default_font", "r", "g", "b"});
+    register_service(&EHMTX::icon_screen, "icon_screen", {"icon_name", "text", "lifetime", "screen_time", "default_font", "r", "g", "b"});
     register_service(&EHMTX::rainbow_icon_screen, "rainbow_icon_screen", {"icon_name", "text", "lifetime", "screen_time", "default_font"});
 
     register_service(&EHMTX::text_screen, "text_screen", {"text", "lifetime", "screen_time", "default_font", "r", "g", "b"});
